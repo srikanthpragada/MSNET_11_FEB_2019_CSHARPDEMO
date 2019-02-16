b8cf172182ccddea3aa91a035b52ed64c92880cb
srikanthpragada <srikanthpragada@gmail.com>
2019-02-16 09:29:13 +0530

    16-FEB

--- Employee.cs
+++ Employee.cs
@@ -12,7 +12,11 @@ namespace csharpdemo
         private int id;
         private string name;
         private int salary;
-
+        private static int hraper = 30;
+        public static int GetHraper()
+        {
+            return hraper; 
+        }
         //Constructor 
         public Employee(int id, string name, int salary)
         {
@@ -20,11 +24,30 @@ namespace csharpdemo
             this.name = name;
             this.salary = salary;
         }
+        public Employee(int id, string name)
+        {
+            this.id = id;
+            this.name = name;
+        }
+        // static constructor 
+        static Employee()
+        {
+            Console.WriteLine("Loading classs");
+        }
+
         public void Print()
         {
             Console.WriteLine(this.id);
             Console.WriteLine(this.name);
-            Console.WriteLine(this.salary);
+            Console.WriteLine(salary); // this.salary
+        }
+        public int GetNetSalary()
+        {
+            return this.salary + (this.salary * hraper / 100);
+        }
+        public void Hike(int amount)
+        {
+            this.salary += amount;
         }
     }
 
@@ -32,11 +55,12 @@ namespace csharpdemo
     {
         static void Main(string[] args)
         {
-            Employee e1;  // Object reference 
-
-            e1 = new Employee(1,"Mr. Bill",50000);  // Instance or Object 
-            e1.Print();  // call method 
-
+            Employee e1 = new Employee(1,"Mr. Bill",50000);  // Instance or Object 
+            Employee e2 = new Employee(2, "Mr. Bill", 50000);
+            e1.Hike(10000);
+            Console.WriteLine(e1.GetNetSalary());
+            Console.WriteLine(Employee.GetHraper());  // Static method 
+            
         }
     }
 

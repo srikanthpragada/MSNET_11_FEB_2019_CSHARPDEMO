9a655b9c84cb49746873da6f2472f6ef9a183172
srikanthpragada <srikanthpragada@gmail.com>
2019-02-23 09:29:25 +0530

    23-FEB

---
+++ AdvFeatures.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharpdemo
+{
+    class AdvFeatures
+    {
+        static void Main(string[] args)
+        {
+            var n = new MyTime { Hours = 10, Mins = 20, Secs = 30 };
+            var person = new { Name = "Abc", Age = 30 };
+            Console.WriteLine(person.Name);
+
+            dynamic d = "Abc";
+            Console.WriteLine(d.Length);
+
+            String name = "C#";
+
+            Console.WriteLine($"Name = {name}");
+
+        }
+    }
+}
---
+++ DelegateDemo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharpdemo
+{
+    class DelegateDemo
+    {
+        static bool IsEven(int n)
+        {
+            return n % 2 == 0;
+        }
+        static void Main(string[] args)
+        {
+            int[] a = { 10, 15, 20, 25, 30 };
+
+            // var res  = Array.FindAll(a, IsEven);
+            var res = Array.FindAll(a, n => n % 2 == 0);
+            foreach (int v in res)
+                Console.WriteLine(v);
+        }
+    }
+}
